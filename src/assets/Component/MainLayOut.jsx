2b1609702a704c27cfe3bf94a1d6490d5d0c1257
Md-shahien-khan <shawonkn58@gmail.com>
2025-06-15 00:59:46 +0100

    added footer

--- src/assets/Component/MainLayOut.jsx
+++ src/assets/Component/MainLayOut.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 
 const MainLayOut = () => {
@@ -8,6 +9,7 @@ const MainLayOut = () => {
         <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
